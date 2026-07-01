295ce48ae72776a418b630fed1167c05eb921812
Saharbanu <saharbanu.a2023cse@sece.ac.in>
2026-07-01 12:13:27 +0530

    first commit

---
+++ ProductInventory.java
@@ -0,0 +1,100 @@
+import java.util.Scanner;
+
+class Product {
+    private String productId;
+    private String productName;
+    private double price;
+    private int stockQuantity;
+
+    public Product(String productId, String productName, double price, int stockQuantity) {
+        this.productId = productId;
+        this.productName = productName;
+
+        if (price < 0)
+            this.price = 0.0;
+        else
+            this.price = price;
+
+        if (stockQuantity < 0)
+            this.stockQuantity = 0;
+        else
+            this.stockQuantity = stockQuantity;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public void setPrice(double price) {
+        if (price < 0)
+            this.price = 0.0;
+        else
+            this.price = price;
+    }
+
+    public void setStockQuantity(int stockQuantity) {
+        if (stockQuantity < 0)
+            this.stockQuantity = 0;
+        else
+            this.stockQuantity = stockQuantity;
+    }
+
+    public void applyDiscount(double percentage) {
+        if (percentage >= 0)
+            price = price - (price * percentage / 100);
+    }
+}
+
+public class ProductInventory {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Product ID: ");
+        String id = sc.nextLine();
+
+        System.out.print("Enter Product Name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter Price: ");
+        double price = sc.nextDouble();
+
+        System.out.print("Enter Stock Quantity: ");
+        int quantity = sc.nextInt();
+
+        System.out.print("Enter Discount Percentage: ");
+        double discount = sc.nextDouble();
+
+        Product p = new Product(id, name, price, quantity);
+
+        p.applyDiscount(discount);
+
+        System.out.println("\nProduct Details");
+        System.out.println("Product ID: " + p.getProductId());
+        System.out.println("Product Name: " + p.getProductName());
+        System.out.println("Price after Discount: " + p.getPrice());
+        System.out.println("Stock Quantity: " + p.getStockQuantity());
+
+        sc.close();
+    }
+}
